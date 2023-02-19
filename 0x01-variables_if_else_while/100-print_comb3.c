e64800b6693c12cb14bfdceb816ca2ff43741dc8
Amir Nabil <76922078+AmiirNabil@users.noreply.github.com>
2023-02-19 14:20:46 -0800

    bonus

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Description: 2 digits possible combinations
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i = '0';
+int d = '0';
+
+while (i <= 9)
+{
+while (d <= 9)
+{
+if (!(i > d || i == d))
+{
+putchar(i);
+putchar(d);
+if (i == '8' && d == '9')
+{
+putchar('\n');
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+d++;
+}
+d = '0';
+i++;
+}
+return (0);
+}
