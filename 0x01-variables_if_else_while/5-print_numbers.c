982a246e7fe34cb449b40c933a462cce6faee981
Amir Nabil <76922078+AmiirNabil@users.noreply.github.com>
2023-02-19 12:46:09 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program main entry point
+ *
+ * Description: prints out base 10 digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  printf("0123456789\n");
+return (0);
+}
