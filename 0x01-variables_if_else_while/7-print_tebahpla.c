1d56392d579d6ce202fac413f75863e0ce0665f9
Amir Nabil <76922078+AmiirNabil@users.noreply.github.com>
2023-02-19 13:20:47 -0800

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - program main entry point
+*
+* Description: smile the mirror
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch='z';
+while (ch >= 'a')
+{
+putchar(ch);
+ch--;
+}
+putchar('\n');
+return (0);
+}
