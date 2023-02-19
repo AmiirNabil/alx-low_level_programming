53735279b1e4c72e71736dec367d3fb4bc5bfd81
Amir Nabil <76922078+AmiirNabil@users.noreply.github.com>
2023-02-19 13:42:03 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - program entry point
+*
+* Description: prints all numbers base 16
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("0123456789abcdef\n");
+return (0);
+}
