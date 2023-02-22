f3f8d5f0f3c50bc4f63c45a5a18c07ec505a12fa
Amir Nabil <76922078+AmiirNabil@users.noreply.github.com>
2023-02-21 16:08:31 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _abs - absolute of an integer
+*
+* Description: provides absolute
+*
+* Return: 0 Success
+*
+* @x: int to be checked
+*/
+int _abs(int x)
+{
+if (x < 0)
+return (-x);
+
+return (x);
+}
