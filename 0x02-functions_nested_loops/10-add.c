d3ef64367ac8254f431013a0703121973177e407
Amir Nabil <76922078+AmiirNabil@users.noreply.github.com>
2023-02-21 17:11:00 -0800

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* add - adds up two numbers
+*
+* Description: adds up to integers
+*
+* @x: first number
+*
+* @y: second number
+*
+* Return : the value
+*/
+
+int add(int x, int y)
+{
+return (x + y);
+}
