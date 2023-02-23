fb2c82062ab75c614b6a26c3781c5da720721fcb
Amir Nabil <76922078+AmiirNabil@users.noreply.github.com>
2023-02-23 14:53:01 -0800

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,18 +16,16 @@ for (i = 1; i <= 100; i++)
 if (i % 3 == 0 && i % 15 != 0)
 {
 printf("Fizz");
-continue;
 }
 else if (i % 5 == 0 && i % 15 != 0)
 {
 printf("Buzz");
-continue;
 }
 else if (i % 15 == 0)
 {
 printf("FizzBuzz");
-continue;
 }
+else
 printf("%d", i);
 
 if (i < 100)
