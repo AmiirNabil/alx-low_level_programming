de03e084be74804b16b2eda9d9c3935cc3d503c2
Amir Nabil <76922078+AmiirNabil@users.noreply.github.com>
2023-02-27 08:36:26 -0800

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * Return: void
+ *
+ * @str: the string to print every other character of
+ */
+
+void puts2(char *str)
+{
+	int len, i;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+	}
+
+	for (i = 0; i < len - 1; i = i + 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
